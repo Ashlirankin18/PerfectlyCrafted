6ef3cbc34ccdc5a4d77a04d70dd6d25a54e49480
Ashlirankin18 <ashlirankin18@gmail.com>
2019-03-04 11:55:18 -0500

    show user hair products in their hair products collection

--- PerfectlyCrafted/Controller/HairProductsTableViewController.swift
+++ PerfectlyCrafted/Controller/HairProductsTableViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import FirebaseFirestore
+
 class HairProductsTableViewController: UITableViewController {
 
   @IBOutlet weak var backButton: UIBarButtonItem!
-
+  private var userProducts = [ProductModel](){
+    didSet{
+      DispatchQueue.main.async {
+        self.tableView.reloadData()
+      }
+    }
+  }
+  private var userSession: UserSession!
+  
   override func viewDidLoad() {
         super.viewDidLoad()
-
+        userSession = AppDelegate.theUser
          self.navigationItem.rightBarButtonItem = self.editButtonItem
+    getUserProducts()
+    self.tableView.dataSource = self
     }
 
   @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
@@ -24,12 +36,39 @@ class HairProductsTableViewController: UITableViewController {
     dismiss(animated: true, completion: nil)
   }
   
-    override func numberOfSections(in tableView: UITableView) -> Int {
-      
-        return 3
+  private func getUserProducts(){
+    if let user = userSession.getCurrentUser(){
+      let documentReference = DataBaseManager.firebaseDB.collection(FirebaseCollectionKeys.products)
+      documentReference.getDocuments { (snapshot, error) in
+        if let error = error {
+          print(error.localizedDescription)
+        }
+        else if let snapshot = snapshot {
+          let qurey = snapshot.query.whereField("userId", isEqualTo: user.uid)
+          qurey.getDocuments(completion: { (snapshot, error) in
+            if let error = error{
+              print(error.localizedDescription)
+            }
+            else if let snapshot = snapshot {
+              let document = snapshot.documents
+              document.forEach{
+               let product =  ProductModel.init(dict: $0.data())
+                self.userProducts.append(product)
+              }
+            }
+          })
+        }
+      }
     }
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    
   }
-
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  return  userProducts.count
+  }
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath)
+    let product = userProducts[indexPath.row]
+    cell.textLabel?.text = product.productName
+    return cell
+  }
 }
--- PerfectlyCrafted/Model/FirebaseProductModel.swift
+++ PerfectlyCrafted/Model/FirebaseProductModel.swift
@@ -9,10 +9,29 @@
 import Foundation
 
 struct ProductModel:Codable {
+  
   let productName: String
   let productId: String
   let productDescription: String
   var userId:String
-  let productImage: String
+  var productImage: String
   let category: String
+init(productName:String,productId:String,productDescription:String,userId:String,productImage:String,category:String){
+  
+    self.productName = productName
+    self.productImage = productImage
+    self.productId = productId
+    self.productDescription = productDescription
+    self.userId = userId
+    self.category = category
+  }
+  init(dict:[String:Any]) {
+    self.productName = dict["productName"] as? String ?? "no product Name found"
+    self.productId = dict["productId"] as? String ?? "no product Id found"
+    self.productDescription = dict["productDescription"] as? String ?? "no product description found"
+    self.userId = dict["userId"] as? String ?? "no user id was found"
+    self.productImage = dict["productImage"] as? String ?? "no product url found"
+    self.category = dict["category"] as? String ?? "no category found"
+  }
 }
+
