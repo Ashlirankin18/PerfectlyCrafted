f9698183fa45bf28ddcdbdd817f5f31a826307dc
AshliRankin <ashlirankin18@gmail.com>
2020-03-19 16:58:10 -0400

    Created a view controller that will display the entry to share

---
+++ PerfectlyCrafted/Post/Post Share/PostShareViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PostShareViewController.swift
+//  PerfectlyCrafted
+//
+//  Created by Ashli Rankin on 3/19/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+final class PostShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
