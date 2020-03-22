4e27396b4bbb3c32c45c305c8a4e90e4bfb36cf5
AshliRankin <ashlirankin18@gmail.com>
2020-03-22 10:56:19 -0400

    Created a new controller for settings

---
+++ PerfectlyCrafted/Settings/Controllers/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  PerfectlyCrafted
+//
+//  Created by Ashli Rankin on 3/22/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+/// `UIViewController` subclass which displays the various app settings.
+final class SettingsViewController: UIViewController {
+    @IBOutlet private weak var colorSelectionCollectionView: UICollectionView!
+}
---
+++ PerfectlyCrafted/Settings/Views/ColorSelectionCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ColorSelectionCollectionViewCell.swift
+//  PerfectlyCrafted
+//
+//  Created by Ashli Rankin on 3/22/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+/// `UICollectionViewCell` subclass which displays the various colors avalible.
+final class ColorSelectionCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet private weak var colorSelectionButton: UIButton!
+    
+    @IBAction private func colorSelectionButtonTapped(_ sender: UIButton) {
+        
+    }
+}
