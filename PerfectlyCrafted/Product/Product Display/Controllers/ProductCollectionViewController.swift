abc20ade444f1b4e33e6569c907d1c8c924f6885
AshliRankin <ashlirankin18@gmail.com>
2020-04-24 19:44:43 -0400

    Added a controller that will be used tp add a product

---
+++ PerfectlyCrafted/Product/Product Display/Controllers/ProductCollectionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProductCollectionViewController.swift
+//  PerfectlyCrafted
+//
+//  Created by Ashli Rankin on 4/24/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewController: UICollectionViewController {
+}
---
+++ PerfectlyCrafted/Product/Product Display/Views/ProductCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.swift
+//  PerfectlyCrafted
+//
+//  Created by Ashli Rankin on 4/24/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
